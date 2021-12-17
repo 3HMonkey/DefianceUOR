1a096e2b7bf4522b4bd9b55e89d28607c4cfa52e
3HMonkey <wewantthejoker@gmail.com>
2021-12-17 15:20:33 +0100

    Added backpack organizer command "[organizeme"

---
+++ Projects/UOContent/Custom/Misc/OganizePouch.cs
@@ -0,0 +1,31 @@
+﻿using Server;
+using Server.Items;
+
+public class OrganizePouch : BaseContainer
+{
+    [Constructible]
+    public OrganizePouch() : base(0xE79)
+    {
+        Weight = 1.0;
+        Name = "Organization Pouch";
+    }
+
+    public OrganizePouch(Serial serial)
+        : base(serial)
+    {
+    }
+
+    public override void Serialize(IGenericWriter writer)
+    {
+        base.Serialize(writer);
+
+        writer.Write(0); // version
+    }
+
+    public override void Deserialize(IGenericReader reader)
+    {
+        base.Deserialize(reader);
+
+        int version = reader.ReadInt();
+    }
+}
