76261a2fbce310785218fdcd382cd1039072ef42
3HMonkey <wewantthejoker@gmail.com>
2021-12-19 13:59:53 +0100

    changes dor game time command

--- Projects/UOContent/Custom/Commands/GameTimeCommand.cs
+++ Projects/UOContent/Custom/Commands/GameTimeCommand.cs
@@ -1,6 +1,23 @@
-﻿namespace Server.Custom.Commands;
+﻿using Server.Mobiles;
 
-public class GameTimeCommand
+
+namespace Server.Commands
 {
-    
+    public static class GameTime
+    {
+        public static void Initialize()
+        {
+            CommandSystem.Register("GameTime", AccessLevel.Player, GameTime_OnCommand);
+        }
+
+        [Usage("GameTime"), Description("Returns the player's game time.")]
+        private static void GameTime_OnCommand(CommandEventArgs e)
+        {
+            if (e.Mobile is PlayerMobile pm)
+            {
+                e.Mobile.SendMessage($"Your game time is: {pm.GameTime}");
+            }
+
+        }
+    }
 }
